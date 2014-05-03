14ee57f97ef5bbd836e884cedfdf6320cb06d144
pseudomuto <david.muto@gmail.com>
2014-05-03 13:54:28 -0400

    removing auth_token from users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,7 @@
 class User < ActiveRecord::Base
-  before_save :ensure_auth_token
-
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :applications, class_name: 'Doorkeeper::Application', as: :owner
-
-  def reset_auth_token
-    update_column(:auth_token, generate_auth_token)
-  end
-
-  private
-
-  def ensure_auth_token
-    self.auth_token = generate_auth_token if auth_token.blank?
-  end
-
-  def generate_auth_token
-    loop do
-      token = Devise.friendly_token
-      break token unless User.where(auth_token: token).first
-    end
-  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,20 +17,4 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 1, user.errors.size
     assert user.errors.has_key?(:password)
   end
-
-  test 'auth_token is generated on save' do
-    user = User.new(email: 'some@user.com', password: 'i13uy6h89')
-    user.save!
-
-    refute user.auth_token.blank?
-  end
-
-  test 'reset_auth_token generates a new token and saves the user' do
-    user = users(:david)
-
-    token = user.auth_token.dup
-    user.reset_auth_token
-
-    refute_equal token, user.reload.auth_token
-  end
 end
