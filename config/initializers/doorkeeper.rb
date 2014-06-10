31a0f31d2f21a508f367e5d219006d652061508f
pseudomuto <david.muto@gmail.com>
2014-06-10 07:28:17 -0400

    accepting client credentials via params as well

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -41,7 +41,7 @@ Doorkeeper.configure do
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
   # falls back to the `:client_id` and `:client_secret` params from the `params` object.
   # Check out the wiki for more information on customization
-  client_credentials :from_basic
+  client_credentials :from_basic, :from_params
 
   # Change the way access token is authenticated from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
