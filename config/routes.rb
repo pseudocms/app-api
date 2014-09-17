6eef222e9a0ba504729be44782ed0f2664b0de1f
pseudomuto <david.muto@gmail.com>
2014-09-17 00:04:03 +0000

    redirecting / to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,13 @@ Rails.application.routes.draw do
     skip_controllers :applications, :authorized_applications
   end
 
-  def devise_options
-    {
-      skip: [:password, :registration],
-      path_names: {
-        sign_in: 'login', sign_out: 'logout'
-      }
-    }
-  end
-
   scope module: :v1, defaults: { format: :json }, constraints: ApiConstraints.new('1', default: true) do
     get '/user', to: 'users#user'
     resources :users, except: [:new, :edit]
     resources :sites, except: [:new, :edit]
   end
+
+  root to: redirect("/admin")
 
   namespace :admin do
     root "dashboard#index"
