c1d37f4998f1dfd1fdcfa3a8215004aa1f423795
pseudomuto <david.muto@gmail.com>
2014-10-17 01:24:00 +0000

    pushing token to local storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :ensure_authed_user
+  helper_method :auth_token
 
   private
+
+  def auth_token
+    session[:auth_token]
+  end
 
   def ensure_authed_user
     redirect_to admin_login_path unless valid_token?
   end
 
   def valid_token?
-    if auth_token = session[:auth_token]
+    if auth_token.present?
       token = Doorkeeper::AccessToken.where(token: auth_token).first
       return token && token.accessible?
     end
